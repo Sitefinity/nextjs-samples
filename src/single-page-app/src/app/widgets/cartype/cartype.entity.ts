f54915f6ff272857b6a2d074e2007981f34a5929
Decho Dechev <decho.dechev@progress.com>
2025-06-30 11:27:46 +0000

    Update 15.2.8433-1066369

--- src/single-page-app/src/app/widgets/cartype/cartype.entity.ts
+++ src/single-page-app/src/app/widgets/cartype/cartype.entity.ts
@@ -1,10 +1,7 @@
-import { Browsable, WidgetEntity } from '@progress/sitefinity-widget-designers-sdk';
+import { WidgetEntity } from '@progress/sitefinity-widget-designers-sdk';
 
 @WidgetEntity('CarType', 'Car type')
 export class CarTypeEntity {
-    @Browsable(false)
     SfFieldType!: string;
-
-    @Browsable(false)
     SfFieldName!: string;
 }
