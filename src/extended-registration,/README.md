3c6b4f31bc572f59d536dc559043edd4d92a4683
Decho Dechev <decho.dechev@progress.com>
2026-01-27 15:45:05 +0000

    Update 15.4.8624-1276090

---
+++ src/extended-registration,/README.md
@@ -0,0 +1,5 @@
+# Sitefinity Renderer for Next.js
+
+Blank starter template project for creating a new NextJS Renderer.
+
+For additional setup instructions, see [Setup](https://github.com/sitefinity/nextjs-samples).
--- src/extended-registration/src/app/widget-registry.ts
+++
@@ -1,8 +0,0 @@
-import { WidgetRegistry, initRegistry, defaultWidgetRegistry, addWidgetViews } from '@progress/sitefinity-nextjs-sdk';
-import { RegistrationCaptchaV2View } from './widgets/registration-captcha-v2/registration-captcha-v2.view';
-import { RegistrationCaptchaV3View } from './widgets/registration-captcha-v3/registration-captcha-v3.view';
-
-addWidgetViews(defaultWidgetRegistry, 'SitefinityRegistration', {'RegistrationCaptchaV2': RegistrationCaptchaV2View});
-addWidgetViews(defaultWidgetRegistry, 'SitefinityRegistration', {'RegistrationCaptchaV3': RegistrationCaptchaV3View});
-
-export const widgetRegistry: WidgetRegistry = initRegistry(defaultWidgetRegistry);
