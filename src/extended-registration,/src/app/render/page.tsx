98d67a4cf7655a460f81982030435e98019d83b0
Nikolay Novkirishki <nnovkiri@progress.com>
2026-01-29 13:42:19 +0200

    Fix sample name

--- src/extended-registration,/src/app/render/page.tsx
+++
@@ -1,5 +0,0 @@
-import { RenderWidget } from '@progress/sitefinity-nextjs-sdk/pages';
-
-export default async function Render({ searchParams }: { searchParams: Promise<{ [key: string]: string }> }) {
-    return RenderWidget({ searchParams });
-}
